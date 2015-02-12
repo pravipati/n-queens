9c8abfad5288005a9c23ce22ee9c59997da5c8ec
pravipati <psravipati@gmail.com>
2015-02-11 19:54:00 -0800

    checkpoint

--- src/solvers.js
+++ src/solvers.js
@@ -15,29 +15,67 @@
 
 window.findNRooksSolution = function(n) {
 
+  //declare finalSolArray
+  var finalSolArray = [];
   //instantiate an empty board
+  var boardx = new Board({n: n});
   //set R1 to (0,0)
+  boardx.togglePiece(0,0);
 
-  //recursive function
-    //input: board obj, numRooks
+  var recurseBoard = function(board, numRooks, n){
+  //recursive function: recurseBoard
+    //input: board obj, numRooks, n
     //output: an array of boards with numRooks+1 rooks
-    //
+    //if numRooks = n,
+      //push the board to finalSolArray
+      //return
+    if (numRooks === n) {
+      finalSolArray.push(board);
+      return;
+    }
     //initialize outputSolArray
+    var  outputSolArray =[];
     //loop through numRooksth row
+    for (var j = 0; j < n; j++) {
       // create a copy of the board
+      var boardArray = arrayTrueCopy(board.rows());
+      var newBoard = new Board(boardArray);
       // add rook to board at (numRooks, j)
+      newBoard.togglePiece(numRooks, j);
       // check for conflict
         // board.hasAny....
-      // if no conflict (all false), then
+      var conflictPool = !newBoard.hasAnyRowConflicts() && !newBoard.hasAnyColConflicts()
+      // if no conflict (all false), then push to outputSolArray
+      if(conflictPool === true){
+        outputSolArray.push(newBoard);
+      }
+    }
+    //for each board in outputSolArray
+    for(var k = 0; k < outputSolArray.length; k++){
+      recurseBoard(outputSolArray[k], numRooks + 1, n);
+    }
+
+    //recurseBoard(outputSolArray[i], numRooks )
+    //
+  };
+
+  recurseBoard(boardx,1,n);
 
 
-  var solution = undefined; //fixme
+
+  var solution = finalSolArray[0].rows(); //fixme
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
-
+window.arrayTrueCopy = function(array){
+  var outputArray = [];
+  for (var i = 0; i < array.length; i++) {
+    outputArray[i] = array[i].slice();
+  }
+  return outputArray;
+};
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
